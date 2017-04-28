bfb6ee4c6c7af1d20a373b78e378f450d16f3831
Dmitry Banasko <dmitry.banasko@revjet.com>
2017-04-28 15:57:26 -0300

    update pb to the latest version 2.3.6

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,26 @@
 FROM armhf/ubuntu:latest
-ENV TERM                    "xterm"
-ENV PhantomBotVersion       "2.3.5.3"
-ENV homeDir                 "/PhantomBot"
+
+ENV TERM="xterm"
+ENV PB_VERSION="2.3.6"
+ENV HOMEDIR="/PhantomBot"
 
 RUN DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y --no-install-recommends \
-openjdk-8-jre-headless \
-unzip \
-curl \
-wget \
-&& apt-get clean \
-&& rm -rf \
-/var/lib/apt/lists/* \
-/var/tmp/*
+  openjdk-8-jre-headless \
+  unzip \
+  curl \
+  wget \
+  && apt-get clean \
+  && rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/*
 
-RUN echo `java -version`
 RUN mkdir -p /PhantomBot /scripts
+
 WORKDIR /PhantomBot
-COPY botlogin.txt /scripts
-COPY entrypoint.sh /entrypoint.sh
+
+COPY ./botlogin.txt /
+COPY ./entrypoint.sh /
+
 RUN chmod 755 /entrypoint.sh
 
 EXPOSE 25003-25005
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
-version=${PhantomBotVersion:="Latest"}
+version=${PB_VERSION:="Latest"}
 name="PhantomBot-${version}"
 
-echo "running entrypoint"
+echo "Running Entrypoint"
 
-if [ ! -f ${homeDir}/botlogin.txt ]; then
-echo "botlogin.txt not found so downloading and configuring the files"
+if [ ! -f ${HOMEDIR}/botlogin.txt ]; then
+  echo "botlogin.txt not found so downloading and configuring the files"
 
-# todo: Remove redundancy
-if [ ${version} == "Latest" ]; then
-            echo "Started downloading PhantomBot version: ${version}"
-            wget --header="User-Agent: Docker-build" -q -O "${homeDir}/Phantombot.zip" $(curl -s https://api.github.com/repos/PhantomBot/PhantomBot/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4)
-            echo "Finished downloading"
-            echo "Unzipping files"
-            unzip "${homeDir}/Phantombot.zip" -d ${homeDir} \
-            && mv ${homeDir}/*/* "${homeDir}" \
-            && rm -rf "${homeDir}/Phantombot.zip"
-                        ls ${homeDir} -all
-        else
-            echo "Started downloading PhantomBot version: ${version}"
-            wget --header="User-Agent: Docker-build" -q -O "${homeDir}/Phantombot.zip" https://github.com/PhantomBot/PhantomBot/releases/download/v${version}/PhantomBot-${version}.zip
-            echo "Finished downloading"
-            echo "Unzipping files"
-            unzip "${homeDir}/Phantombot.zip" -d ${homeDir} \
-            && mv ${homeDir}/*/* "${homeDir}" \
-            && rm -rf "${homeDir}/Phantombot.zip"
-            ls ${homeDir} -all
-fi
-    echo "creating botlogin.txt"
-    cp /scripts/botlogin.txt ${homeDir} && sed -i "s/\%BOT_USERNAME\%/${BOT_USERNAME}/g;s/\%BOT_OAUTH\%/${BOT_OAUTH}/g;s/\%BOT_CHANNEL\%/${BOT_CHANNEL}/g;s/\%BOT_OWNER\%/${BOT_OWNER}/g;" "${homeDir}/botlogin.txt"
-    echo "starting bot"
-    chmod +x ${homeDir}/launch-service.sh
-    ${homeDir}/launch-service.sh
+  # todo: Remove redundancy
+  if [ ${version} == "Latest" ]; then
+    echo "Started downloading PhantomBot version: ${version}"
+    wget --header="User-Agent: Docker-build" -q -O "${HOMEDIR}/Phantombot.zip" $(curl -s https://api.github.com/repos/PhantomBot/PhantomBot/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4)
+    echo "Finished downloading"
+    echo "Unzipping files"
+    unzip "${HOMEDIR}/Phantombot.zip" -d ${HOMEDIR} \
+    && mv ${HOMEDIR}/*/* "${HOMEDIR}" \
+    && rm -rf "${HOMEDIR}/Phantombot.zip"
+    ls ${HOMEDIR} -all
+  else
+    echo "Started downloading PhantomBot version: ${version}"
+    wget --header="User-Agent: Docker-build" -q -O "${HOMEDIR}/Phantombot.zip" https://github.com/PhantomBot/PhantomBot/releases/download/v${version}/PhantomBot-${version}.zip
+    echo "Finished downloading"
+    echo "Unzipping files"
+    unzip "${HOMEDIR}/Phantombot.zip" -d ${HOMEDIR} \
+    && mv ${HOMEDIR}/*/* "${HOMEDIR}" \
+    && rm -rf "${HOMEDIR}/Phantombot.zip"
+    ls ${HOMEDIR} -all
+  fi
+
+  echo "Creating botlogin.txt"
+  cp /scripts/botlogin.txt ${HOMEDIR} && sed -i "s/\%BOT_USERNAME\%/${BOT_USERNAME}/g;s/\%BOT_OAUTH\%/${BOT_OAUTH}/g;s/\%BOT_CHANNEL\%/${BOT_CHANNEL}/g;s/\%BOT_OWNER\%/${BOT_OWNER}/g;" "${HOMEDIR}/botlogin.txt"
+  echo "Starting Bot"
+  cat ${HOMEDIR}/botlogin.txt
+  chmod +x ${HOMEDIR}/launch-service.sh
+  ${HOMEDIR}/launch-service.sh
 else
-echo "Found install, starting"
-    ${homeDir}/launch-service.sh
+  echo "Found install, starting"
+  cat ${HOMEDIR}/botlogin.txt
+  ${HOMEDIR}/launch-service.sh
 fi
